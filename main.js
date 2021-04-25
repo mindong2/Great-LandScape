33624688446422fb530661313f28c61b39e1c37f
mindong2 <qlsnaos12@naver.com>
2021-04-25 21:54:33 +0900

    Mobile menu create

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const header = document.querySelector(`header`);
-
+const hamberger = document.querySelector(`.hamberger`);
+const navbar = document.querySelector(`.navbar`);
 
 function scrollEvent(){
- header.classList.toggle(`sticky`, window.scrollY > 0);
+    header.classList.toggle(`sticky`,scrollY>0 );
 }
 
+
+function viewMenu(){
+    navbar.classList.toggle(`view`);
+    header.classList.toggle(`color`)
+}
+
+hamberger.addEventListener(`click`, viewMenu);
 window.addEventListener("scroll",scrollEvent);
